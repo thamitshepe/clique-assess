995e0e046da1c946e715e796bff5a6ce7e792f0e
thamitshepe <ttshepe2002@gmail.com>
2024-05-22 06:54:36 +0200

    Beta

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,23 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import image from '@rollup/plugin-image';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // Other Vite config options...
 
+  plugins: [
+    react(), // Make sure to add the React plugin here
+    image()  // Then add the image plugin here
+  ],
+
   build: {
     rollupOptions: {
-      plugins: [
-        image(),
-        [react()] // Add the image plugin here
-      ],
       external: [
         // Add any other external dependencies here
       ]
     }
   }
 });
-
-
-
