5eda935753e51c96eddfdf4bc64d37da75737e60
thamitshepe <ttshepe2002@gmail.com>
2024-05-22 10:22:51 +0200

    Beta

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import 'normalize.css';
 import './css/style.css';
 import './css/montserrat.css';
 import 'jsvectormap/dist/css/jsvectormap.css';
