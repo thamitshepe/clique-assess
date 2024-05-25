0111d2b8c4b04231e510db9ef0cdd0a35be04887
thamitshepe <ttshepe2002@gmail.com>
2024-05-25 18:30:00 +0200

    Beta

--- app/src/images/nba/index.ts
+++ app/src/images/nba/index.ts
@@ -1,39 +1,39 @@
 // images/football/leagues/index.ts
-
 import NBA from './NBA.svg';
-import atlanta_hawks from './atlanta-hawks.svg'
-import boston_celtics from './boston-celtics.svg'
-import brooklyn_nets from './brooklyn-nets.svg'
-import charlotte_hornets from './charlotte-hornets.svg'
-import chicago_bulls from './chicago-bulls.svg'
-import cleveland_cavaliers from './cleveland-cavaliers.svg'
-import dallas_mavericks from './dallas-mavericks.svg'
-import denver_nuggets from './denver-nuggets.svg'
-import detroit_pistons from './detroit-pistons.svg'
-import golden_state_warriors from './golden-state-warriors.svg'
-import houston_rockets from './houston-rockets.svg'
-import indiana_pacers from './indiana-pacers.svg'
-import la_clippers from './la-clippers.svg'
-import los_angeles_lakers from './los-angeles-lakers.svg'
-import memphis_grizzlies from './memphis-grizzlies.svg'
-import miami_heat from './miami-heat.svg'
-import milwaukee_bucks from './milwaukee-bucks.svg'
-import minnesota_timberwolves from './minnesota-timberwolves.svg'
-import new_orleans_pelicans from './new-orleans-pelicans.svg'
-import new_york_knicks from './new-york-knicks.svg'
-import oklahoma_city_thunder from './oklahoma-city-thunder.svg'
-import orlando_magic from './orlando-magic.svg'
-import philadelphia_76ers from './philadelphia-76ers.svg'
-import phoenix_suns from './phoenix-suns.svg'
-import portland_trail_blazers from './portland-trail-blazers.svg'
-import sacramento_kings from './sacramento-kings.svg'
-import san_antonio_spurs from './san-antonio-spurs.svg'
-import toronto_raptors from './toronto-raptors.svg'
-import utah_jazz from './utah-jazz.svg'
-import washington_wizards from './washington-wizards.svg'
+import Atlanta_Hawks from './atlanta-hawks.svg';
+import Boston_Celtics from './boston-celtics.svg';
+import Brooklyn_Nets from './brooklyn-nets.svg';
+import Charlotte_Hornets from './charlotte-hornets.svg';
+import Chicago_Bulls from './chicago-bulls.svg';
+import Cleveland_Cavaliers from './cleveland-cavaliers.svg';
+import Dallas_Mavericks from './dallas-mavericks.svg';
+import Denver_Nuggets from './denver-nuggets.svg';
+import Detroit_Pistons from './detroit-pistons.svg';
+import Golden_State_Warriors from './golden-state-warriors.svg';
+import Houston_Rockets from './houston-rockets.svg';
+import Indiana_Pacers from './indiana-pacers.svg';
+import La_Clippers from './la-clippers.svg';
+import Los_Angeles_Lakers from './los-angeles-lakers.svg';
+import Memphis_Grizzlies from './memphis-grizzlies.svg';
+import Miami_Heat from './miami-heat.svg';
+import Milwaukee_Bucks from './milwaukee-bucks.svg';
+import Minnesota_Timberwolves from './minnesota-timberwolves.svg';
+import New_Orleans_Pelicans from './new-orleans-pelicans.svg';
+import New_York_Knicks from './new-york-knicks.svg';
+import Oklahoma_City_Thunder from './oklahoma-city-thunder.svg';
+import Orlando_Magic from './orlando-magic.svg';
+import Philadelphia_76ers from './philadelphia-76ers.svg';
+import Phoenix_Suns from './phoenix-suns.svg';
+import Portland_Trail_Blazers from './portland-trail-blazers.svg';
+import Sacramento_Kings from './sacramento-kings.svg';
+import San_Antonio_Spurs from './san-antonio-spurs.svg';
+import Toronto_Raptors from './toronto-raptors.svg';
+import Utah_Jazz from './utah-jazz.svg';
+import Washington_Wizards from './washington-wizards.svg';
+
 
 
 // Import other SVG files here
 
-export { NBA, atlanta_hawks, boston_celtics, brooklyn_nets, charlotte_hornets, chicago_bulls, cleveland_cavaliers, dallas_mavericks, denver_nuggets, detroit_pistons, golden_state_warriors, houston_rockets, indiana_pacers, la_clippers, los_angeles_lakers, memphis_grizzlies, miami_heat, milwaukee_bucks, minnesota_timberwolves, new_orleans_pelicans, new_york_knicks, oklahoma_city_thunder, orlando_magic, philadelphia_76ers, phoenix_suns, portland_trail_blazers, sacramento_kings, san_antonio_spurs, toronto_raptors, utah_jazz, washington_wizards };
+export { NBA, Atlanta_Hawks, Boston_Celtics, Brooklyn_Nets, Charlotte_Hornets, Chicago_Bulls, Cleveland_Cavaliers, Dallas_Mavericks, Denver_Nuggets, Detroit_Pistons, Golden_State_Warriors, Houston_Rockets, Indiana_Pacers, La_Clippers, Los_Angeles_Lakers, Memphis_Grizzlies, Miami_Heat, Milwaukee_Bucks, Minnesota_Timberwolves, New_Orleans_Pelicans, New_York_Knicks, Oklahoma_City_Thunder, Orlando_Magic, Philadelphia_76ers, Phoenix_Suns, Portland_Trail_Blazers, Sacramento_Kings, San_Antonio_Spurs, Toronto_Raptors, Utah_Jazz, Washington_Wizards };
 // Export other SVG files here
